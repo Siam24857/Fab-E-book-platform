d4b4c60c378e4f2d9b5657cb98f92d1b637360e2
Siam24857 <siamtechofficial1597@gmail.com>
2026-06-23 23:24:17 +0600

    all is done

--- src/app/Dashboard/reader/historyboks/page.jsx
+++ src/app/Dashboard/reader/historyboks/page.jsx
@@ -1,17 +1,22 @@
-import { userdata, userhistory, userseissondata } from '@/app/lib/Action/Userinfo';
-import React from 'react';
-import PurchaseHistoryPage from './Allhistorybooks';
+// ./src/app/Dashboard/reader/historyboks/page.jsx
+// গুরুত্বপূর্ণ: এই ফাইলের শুরুতে "use client" থাকলে সেটি সরিয়ে ফেলুন
+
+import { userdata, userhistory, userseissondata } from "@/app/lib/Action/Userinfo";
+import PurchaseHistoryPage from "./PurchaseHistoryPage";
  
 
-const ALlhistory = async() => {
-     const userData = await userdata();
-      const tokenData = await userseissondata();
-      const historyData = await userhistory(tokenData, userData.id);
-  return (
-    <div>
-      <PurchaseHistoryPage historyData={historyData} tokenData={tokenData} userData={userData} ></PurchaseHistoryPage>
-    </div>
-  );
-};
+export default async function HistoryBooksPage() {
+  // সার্ভার সাইডে ডেটা ফেচ করুন
+  const userData = await userdata();
+  const tokenData = await userseissondata();
+  const historyData = await userhistory(tokenData, userData.id);
 
-export default ALlhistory;
+  // ডেটা ক্লায়েন্ট কম্পোনেন্টে পাঠান
+  return (
+    <PurchaseHistoryPage
+      historyData={historyData} 
+      tokenData={tokenData} 
+      userData={userData} 
+    />
+  );
+}
